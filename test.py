36e6ddb93ca3ffd6214bc7028979cf0ee9abbb86
Biki Kumar <77744051+Vicky-kr@users.noreply.github.com>
2025-02-28 18:28:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 with open('test/input.json','r') as f:
   with open('test/output.xml','w') as file:
-    file.write('<status_code>1000</status_code><response>ok</response>')
+    file.write('<status_code>1000</status_code><response>ok</response><data><item>test</item></data>')
